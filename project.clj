7327cabbf284c36e35b3eefcea9e3f39c6aab214
sivakumargsk <geddasiva777@gmail.com>
2016-01-25 12:13:54 +0530

    Some dependencies are added

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-  				 [org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojure "1.7.0"]
                  ;;Backend dependencies
                  [compojure "1.4.0"]
                  [secretary "1.2.3"]
@@ -16,11 +16,13 @@
                  [ring/ring-core "1.4.0" :exclusions [javax.servlet/servlet-api]]
                  [ring/ring-servlet "1.4.0" :exclusions [javax.servlet/servlet-api]]
                  [ring/ring-defaults "0.1.5" :exclusions [javax.servlet/servlet-api]]
-                 [cc.qbits/jet "0.7.1"]
+                 [cc.qbits/jet "0.7.3"]
                  [clj-time "0.11.0"]
                  [bouncer "1.0.0"]
                  [reagent "0.5.1"]
-                ]
+                 [cljs-ajax "0.5.3"]
+                 [noir-exception "0.2.5"]
+                 ]
   :source-paths ["src/clj"]
   :main react-cljs.core
   :plugins [
@@ -28,10 +30,10 @@
             [migratus-lein "0.1.7"]
             [lein-cljsbuild "1.1.2"]
             ]
-    :cljsbuild {:builds
-                [{:id "app"
-                  :source-paths ["src/cljs"]
-                  :compiler {:output-to "resources/public/js/app.js"
+  :cljsbuild {:builds
+              [{:id "app"
+                :source-paths ["src/cljs"]
+                :compiler {:output-to "resources/public/js/app.js"
                            :output-dir "resources/public/js/out"
                            :source-map true
                            :optimizations :none
