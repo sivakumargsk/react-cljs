df61668cbcbc07a14180bafa700c616d0b87b926
sivakumargsk <geddasiva777@gmail.com>
2016-01-25 11:43:20 +0530

    single page app client side form code is created

--- src/cljs/react_cljs/core.cljs
+++ src/cljs/react_cljs/core.cljs
@@ -1,21 +1,74 @@
 (ns react-cljs.core
-  (:require-macros [secretary.core :refer [defroute]])
   (:require [goog.events :as events]
             [goog.dom :as dom]
             [goog.history.EventType :as EventType]
-            [bouncer.core :as b]
-            [bouncer.validators :as v]
-            [secretary.core :as secretary]
-            [reagent.core :as reagent :refer [atom render]])
-  (:import goog.History))
+            [reagent.core :as reagent :refer [atom render]]))
+
+
+(def state (atom {:doc {} :saved? false}))
+
+(defn set-value! [id value]
+  (swap! state assoc :saved? false)
+  (swap! state assoc-in [:doc id] value))
+
+(defn get-value [id]
+  (get-in @state [:doc id]))
+
+;; ------------------------------------------------------------------
+;; elements
+
+(defn row [label & body]
+  [:div.row
+   [:div.col-md-2 [:label label]]
+   [:div.col-md-3 body]])
+
+(defn text-input [id label]
+  [row label
+   [:input.form-control {:type "text"
+                         :value (get-value id)
+                         :on-change #(set-value! id (-> % .-target .-value))}]])
+
+
+(defn list-item [id k v selections]
+  (letfn [(handle-click! []
+            (swap! selections update-in [k] not)
+            (set-value! id (->> @selections
+                                (filter second)
+                                (map first))))]
+    [:li {:class (str "list-group-item"
+                      (if (k @selections)  " active"))
+          :on-click handle-click!}
+     v]))
+
+(defn selection-list [id label & items]
+  (let [selections (->> items (map (fn [[k]] [k false])) (into {}) atom)]
+    (fn []
+      [:div.row
+       [:div.col-md-2 [:span label]]
+       [:div.col-md-5
+        [:div.row
+         (for [[k v] items]
+           [list-item id k v selections])]]])))
+
+;; ---------------------------------------------------------------------------------
+;; pages
 
 (defn home []
-  [:div.pageheader
-   [:h2 "Hello World"]])
+  [:div
+   [:div.page-header [:h1 "Reagent Form"]]
+   [text-input :first-name "First name"]
+   [text-input :last-name "Last Name"]
 
-(defn render-sample []
+   [selection-list :favorite-drinks "Favorite drinks"
+    [:coffee "Coffee"]
+    [:beer "Beer"]
+    [:crab-juice "Crab juice"]]
+
+   [:button.btn.btn-primary {:type "submit"
+                             :on-click #(.log js/console (clj->js @state))}
+    "Submit"]])
+
+(defn render-home []
   (reagent/render-component [home]
                             (.getElementById js/document "app")))
-
-
-(render-sample)
+(render-home)
