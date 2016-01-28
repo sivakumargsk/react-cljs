c07a4d6b00d68a76bb9c231970e12e8eb62a170e
sivakumargsk <geddasiva777@gmail.com>
2016-01-28 19:34:14 +0530

    react-bootstrap branch is created with some react-bootstrap elements functions

--- src/cljs/react_cljs/core.cljs
+++ src/cljs/react_cljs/core.cljs
@@ -10,15 +10,136 @@
             [cljsjs.react-bootstrap])
   (:import goog.History))
 
+;; =============================== Button ===============================================
+
+(def button-tool-bar (reagent/adapt-react-class (aget js/ReactBootstrap "ButtonToolbar")))
 (def button (reagent/adapt-react-class (aget js/ReactBootstrap "Button")))
 
-(defn home []
+(defn buttons []
   [:div
-   [:h2 "A sample title"]
-   [button {:bsStyle "primary"} "with a button"]])
+   [:div.page-header [:h3[:b "Buttons"]]]
+   [button-tool-bar
+    [button "Default"]
+    [button {:bs-style "primary"} "Primary"]
+    [button {:bs-style "success"} "Success"]
+    [button {:bs-style "info"} "info"]
+    [button {:bs-style "warning"} "Warning"]
+    [button {:bs-style "danger"} "Danger"]
+    [button {:bs-style "link"} "Link"]]])
+
+
+;; ================================ Button-Size ==========================================
+(defn buttons-size []
+  [:div
+   [:div.page-header [:h3 [:b "Button-size"]]]
+   [:div
+    [button-tool-bar
+     [button {:bs-style "primary"
+              :bs-size "large"} "Large button"]
+     [button {:bs-size "large"} "Large button"]]
+    [button-tool-bar
+     [button {:bs-style "primary"} "Default button"]
+     [button  "Default button"]]
+    [button-tool-bar
+     [button {:bs-style "primary"
+              :bs-size "small"} "Small button"]
+     [button {:bs-size "small"} "small button"]]
+    [button-tool-bar
+     [button {:bs-style "primary"
+              :bs-size "xsmall"} "Extra small button"]
+     [button {:bs-size "xsmall"} "Extra small button"]]]])
+
+;; ================================== Block-level-buttons ====================================
+
+(defn block-level-buttons []
+  [:div
+   [:div.page-header [:h3 [:b "Block-level-Buttons"]]]
+   [:div {:class "well"
+          :style {:max-width 400 :margin "0 auto 10px"}}
+    [button {:bs-style "primary" :bs-size "large" :block true}  "Block level button"]
+    [button {:bs-size "large" :block true} "Block level buttons"]]])
+
+;; ==================================== Buttons-Active-State ==================================
+
+(defn active-buttons []
+  [:div
+   [:div.page-header [:h3 [:b "Active-Button"]]]
+   [button-tool-bar
+    [button {:bs-style "primary" :active true}  "Primary button"]
+    [button {:active true} "Button"]]])
+
+;; ==================================== Disabled Buttons =======================================
+
+(defn disabled-buttons []
+  [:div
+   [:div.page-header [:h3 [:b "Disabled-Buttons"]]]
+   [button-tool-bar
+    [button {:bs-style "primary" :disabled true}  "Primary button"]
+    [button {:disabled true} "Button"]]])
+
+;; ===================================== Button tags ============================================
+
+(defn button-tag []
+  [:div
+   [:div.page-header [:h3 [:b "Button-tag"]]]
+   [button-tool-bar
+    [button {:href "#"}  "link"]
+    [button "Button"]]])
+
+;; ===================================== Button loading sate =====================================
+
+;; (defonce timer (reagent/atom (js/Date.)))
+
+;; (defonce time-updater (js/setInterval
+;;                        #(reset! timer (js/Date.)) 1000))
+
+
+(defn button-loading-state []
+  [:div
+   [:div.page-header [:h3 [:b "Button-loading-state not-working"]]]
+   [button {:bs-style "primary"
+            ;; :disabled
+            ;; :on-click
+            } "loading button"]])
+
+;; ======================================= Button Groups =========================================
+
+(def button-group (reagent/adapt-react-class (aget js/ReactBootstrap "ButtonGroup")))
+
+(defn button-groups []
+  [:div
+   [:div.page-header [:h3 [:b "Button-groups"]]]
+   [button-group
+    [button "Left"]
+    [button "middle"]
+    [button "right"]]])
+
+;; ======================================= Button-toolbar-group ===================================
+
+(defn button-toolbar-group []
+  [:div
+   [:div.page-header [:h3 [:b "Button-toolbar-group"]]]
+   [button-tool-bar
+    [button-group
+     [button "Left"]
+     [button "middle"]
+     [button "right"]]]
+   ])
+
+(defn home []
+  [:div.container
+   [:div.page-header
+    [:h2 "React-BootStrap"]]
+   [buttons]
+   [buttons-size]
+   [block-level-buttons]
+   [active-buttons]
+   [disabled-buttons]
+   [button-tag]
+   [button-loading-state]
+   [button-groups]])
 
 (defn render-sample []
   (reagent/render-component [home]
                             (.getElementById js/document "app")))
-
 (render-sample)
