51866e3c612d068f52738351cf069cf6842ba900
sivakumargsk <geddasiva777@gmail.com>
2016-01-28 12:22:44 +0530

    adding react-bootstrap and cljs dependencies

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
 (defproject react-cljs "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "React-bootstrap-elements"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-  				 [org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojure "1.7.0"]
                  ;;Backend dependencies
                  [compojure "1.4.0"]
                  [secretary "1.2.3"]
 
                  ;; Frontend dependencies
-                 [org.clojure/clojurescript "1.7.170"]
+                 [org.clojure/clojurescript "0.0-3211"]
                  [hiccup "1.0.5"]
                  [prismatic/dommy "1.1.0"]
                  [ring/ring-core "1.4.0" :exclusions [javax.servlet/servlet-api]]
@@ -20,7 +20,8 @@
                  [clj-time "0.11.0"]
                  [bouncer "1.0.0"]
                  [reagent "0.5.1"]
-                ]
+                 [cljsjs/react-bootstrap "0.25.1-0"]
+                 ]
   :source-paths ["src/clj"]
   :main react-cljs.core
   :plugins [
@@ -28,10 +29,10 @@
             [migratus-lein "0.1.7"]
             [lein-cljsbuild "1.1.2"]
             ]
-    :cljsbuild {:builds
-                [{:id "app"
-                  :source-paths ["src/cljs"]
-                  :compiler {:output-to "resources/public/js/app.js"
+  :cljsbuild {:builds
+              [{:id "app"
+                :source-paths ["src/cljs"]
+                :compiler {:output-to "resources/public/js/app.js"
                            :output-dir "resources/public/js/out"
                            :source-map true
                            :optimizations :none
