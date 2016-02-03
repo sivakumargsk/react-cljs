498fd4238c7a6315901482b2a6b8c87064e5fd60
sivakumargsk <geddasiva777@gmail.com>
2016-02-03 14:20:06 +0530

    some dependencies are added

--- project.clj
+++ project.clj
@@ -4,14 +4,16 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-  				 [org.clojure/clojure "1.7.0"]
-                 ;;Backend dependencies
+                 [org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
                  [secretary "1.2.3"]
-
-                 ;; Frontend dependencies
-                 [org.clojure/clojurescript "1.7.170"]
+                 [org.clojure/clojurescript "1.7.228" :scope "provided"]
+                 [reagent "0.5.1"
+                  :exclusions [org.clojure/tools.reader]]
                  [hiccup "1.0.5"]
+                 [reagent-forms "0.5.13"]
+                 [reagent-utils "0.1.7"]
+                 [ring "1.4.0"]
                  [prismatic/dommy "1.1.0"]
                  [ring/ring-core "1.4.0" :exclusions [javax.servlet/servlet-api]]
                  [ring/ring-servlet "1.4.0" :exclusions [javax.servlet/servlet-api]]
@@ -19,8 +21,9 @@
                  [cc.qbits/jet "0.7.1"]
                  [clj-time "0.11.0"]
                  [bouncer "1.0.0"]
-                 [reagent "0.5.1"]
-                ]
+                 [venantius/accountant "0.1.6"
+                  :exclusions [org.clojure/tools.reader]]
+                 ]
   :source-paths ["src/clj"]
   :main react-cljs.core
   :plugins [
@@ -28,10 +31,10 @@
             [migratus-lein "0.1.7"]
             [lein-cljsbuild "1.1.2"]
             ]
-    :cljsbuild {:builds
-                [{:id "app"
-                  :source-paths ["src/cljs"]
-                  :compiler {:output-to "resources/public/js/app.js"
+  :cljsbuild {:builds
+              [{:id "app"
+                :source-paths ["src/cljs"]
+                :compiler {:output-to "resources/public/js/app.js"
                            :output-dir "resources/public/js/out"
                            :source-map true
                            :optimizations :none
