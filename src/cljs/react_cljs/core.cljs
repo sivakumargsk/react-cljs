39bbba2c678c8d58fa54e913903c83d204cdb236
sivakumargsk <geddasiva777@gmail.com>
2016-02-10 18:32:01 +0530

    to-do task form is created

--- src/cljs/react_cljs/core.cljs
+++ src/cljs/react_cljs/core.cljs
@@ -3,15 +3,58 @@
   (:require [goog.events :as events]
             [goog.dom :as dom]
             [goog.history.EventType :as EventType]
-            [bouncer.core :as b]
-            [bouncer.validators :as v]
             [secretary.core :as secretary]
             [reagent.core :as reagent :refer [atom render]])
   (:import goog.History))
 
+
+(def all-tasks (atom [{:task-no 1
+                       :task-name "Learning Clojure from ground up"
+                       :completed? false}
+                      {:task-no 2
+                       :task-name "Learning bash shell commands"
+                       :completed? false}]))
+
+
+(def p-style {:font-size "20px"
+              :font-weight "bold"
+              :color "grey"
+              :font-style "italic"
+              :font-family {:align "center"}
+              :text-decoration "line-through"})
+
+
+(def todo-list  (reagent/atom [{:task-no 1 :task-name "Learning Clojure From Ground up" :done false}
+                     {:task-no 2 :task-name "Four Clojure 1 to 10 Tasks" :done false}
+                     {:task-no 3 :task-name "Learning bash Commands" :done false}]))
+
+
+(defn tasks-list [list]
+  [:div
+  (for [item list]
+    ^{:key (:task-no item)}
+    
+    [:div.row
+     [:div.col-sm-1 [:input {:type "checkbox" :style {:width "25px" :height "25px"}}]]
+     [:div.col-sm-9 [:p {:style p-style}  (:task-name item)]]
+     [:div.col-sm-2 [:button.btn.btn-default
+                     [:span.glyphicon.glyphicon-remove {:aria-hidden true}]]]])])
+
+(defn head []
+  [:div.form-group
+   [:div.row
+     [:div.col-sm-10 [:input.form-control {:type "text"}]]
+     [:div.col-sm-2 [:button.btn.btn-primary {} "Add"]]]])
+
 (defn home []
-  [:div.pageheader
-   [:h2 "Hello World"]])
+  [:div.container
+   [:div.col-sm-8
+    [:div.page-header [:h2 "To Do List"]]
+    [head]
+    [:div
+     [:hr]
+     [tasks-list todo-list]
+     [:hr]]]])
 
 (defn render-sample []
   (reagent/render-component [home]
