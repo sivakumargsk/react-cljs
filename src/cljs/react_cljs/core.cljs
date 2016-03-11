0268f209a0a6296a0db16a254a4ed1baa6b2cd52
sivakumargsk <geddasiva777@gmail.com>
2016-03-11 17:12:18 +0530

    Implemented reagent examples in re-frame

--- src/cljs/react_cljs/core.cljs
+++ src/cljs/react_cljs/core.cljs
@@ -1,121 +1,160 @@
 (ns react-cljs.core
   (:require-macros [reagent.ratom :refer [reaction]])
-  (:require [reagent.core :as reagent]
+  (:require [goog.events :as events]
+            [goog.dom :as dom]
+            [goog.history.EventType :as EventType]
+            [reagent.core :as reagent]
             [re-frame.core :refer [register-handler
                                    path
                                    register-sub
                                    dispatch
                                    dispatch-sync
                                    subscribe]]))
+(def initial-state {:click-count 0
+                    :value "foo"
+                    :tasks {1 {:task-name "Learning Re-frame Framework" :done false}
+                            2 {:task-name "Implementing Reagent Examples on re-frame" :done false}
+                            3 {:task-name "Doing To-Do List" :done false}}
+                    })
 
-;; trigger a dispatch every second
-(defonce time-updater (js/setInterval
-                        #(dispatch [:timer (js/Date.)]) 1000))
-
-(def initial-state
-  {:timer (js/Date.)
-   :time-color "#f34"})
-
-
-;; -- Event Handlers ----------------------------------------------------------
-
-
-(register-handler                 ;; setup initial state
-  :initialize                     ;; usage:  (dispatch [:initialize])
-  (fn
-    [db _]
-    (merge db initial-state)))    ;; what it returns becomes the new state
-
+;; Event handler ------------------------
 
 (register-handler
-  :time-color                     ;; usage:  (dispatch [:time-color 34562])
-  (path [:time-color])            ;; this is middleware
-  (fn
-    [time-color [_ value]]        ;; path middleware adjusts the first parameter
-    value))
-
+ :initialize
+ (fn
+   [db _]
+   (merge db initial-state)))
 
 (register-handler
-  :timer
-  (fn
-    ;; the first item in the second argument is :timer the second is the
-    ;; new value
-    [db [_ value]]
-    (assoc db :timer value)))    ;; return the new version of db
+ :change-click-count
+ (fn
+   [db [_ value]]
+   (assoc db :click-count value)))
 
+(register-handler
+ :change-value
+ (fn [db [_ newvalue]]
+   (assoc db :value newvalue)))
 
-;; -- Subscription Handlers ---------------------------------------------------
+(register-handler
+ :add-task
+ (fn [db [_ key name]]
+   (assoc-in db [:tasks key] {:task-name name :done false})))
 
+(register-handler
+ :task-done
+ (fn
+   [db [_ key]]
+   (update-in db [:tasks key :done] not)))
+
+(register-handler
+ :remove-task
+ (fn
+   [db [_ id]]
+   (update-in db [:tasks] dissoc id)))
+
+;; Subscription Handlers -------------------
 
 (register-sub
-  :timer
-  (fn
-    [db _]                       ;; db is the app-db atom
-    (reaction (:timer @db))))    ;; wrap the computation in a reaction
-
+ :count
+ (fn
+   [db _]
+   (reaction (:click-count @db))))
 
 (register-sub
-  :time-color
-  (fn
-    [db _]
-    (reaction (:time-color @db))))
+ :value
+ (fn
+   [db _]
+   (reaction (:value @db))))
 
+(register-sub
+ :tasks
+ (fn [db _]
+   (reaction (:tasks @db))))
 
-;; -- View Components ---------------------------------------------------------
+;; View Components -------------------------
 
-(defn greeting
-  [message]
-  [:h1 message])
-
-
-(defn clock
-  []
-  (let [time-color (subscribe [:time-color])
-        timer (subscribe [:timer])]
-
-    (fn clock-render
-        []
-        (let [time-str (-> @timer
-                           .toTimeString
-                           (clojure.string/split " ")
-                           first
-                           )
-              style {:style {:color @time-color}}]
-             [:div.page-header style [:h2 time-str]]))))
-
-
-(defn color-input
-  []
-  (let [time-color (subscribe [:time-color])]
-
+(defn counting-component []
+  (let [val (subscribe [:count])]
     (fn
       []
-      [:div
-       "Time color: "
-       [:div.row
-       [:div.col-sm-8
-        [:input.form-control {:type "text"
-                              :value @time-color
-                              :on-change #(dispatch
-                                           [:time-color (-> % .-target .-value)])}]]
-       [:div.col-sm-4
-        [:button.btn.btn-primary {:on-click #(dispatch [:initialize])} "Initial Color"]]]])))
-  
-  (defn simple-example
-  []
+      [:div.form-group
+       [:label "The atom Click Count Has Value: " @val]
+       [:br]
+       [:button {:on-click #(dispatch [:change-click-count (swap! val inc)])} "Click Me"]])))
+
+(defn input-changing []
+  (let [val (subscribe [:value])]
+    (fn []
+      [:div.form-group
+       [:label "The value is now: " @val]
+       [:br]
+       [:label "Change Here "
+        [:input {:type "text"
+                 :value @val
+                 :on-change #(dispatch [:change-value (reset! val (-> % .-target .-value))])}]]])))
+
+(defn max-coll [coll]
+  (reduce (fn [x y]
+            (if (> x y) x y)) coll))
+
+
+(defn get-val [id]
+  (.-value (dom/getElement id)))
+
+(defn set-val [id val]
+  (set! (.-value (dom/getElement id)) val))
+
+
+(defn to-do []
+  (let [tasks (subscribe [:tasks])]
+    (fn []
+      (let [task-no (inc (max-coll (keys @tasks)))]
+        [:div
+         [:div.form-group
+          [:div.row
+           [:div.col-sm-10
+            [:input.form-control {:id "add"
+                                  :type "text"
+                                  :placeholder "Enter Your tasks Here"}]]
+           [:div.col-sm-2
+            [:button.btn.btn-primary {:on-click  #(do
+                                                    (dispatch [:add-task task-no (get-val "add")])
+                                                    (set-val "add" "")) } "Add New"]]]]
+         (for [t @tasks]
+           ^{:key (first t)}
+           [:div.form-group
+            [:div.row
+             [:div.col-sm-2
+              [:input
+               {:id (first t)
+                :type "checkbox"
+                :style {:width "25px" :height "25px"}
+                :on-change #(dispatch [:task-done (js/parseInt (-> % .-target .-id))])}]]
+             [:div.col-sm-6
+              [:strong (when (:done (second t))
+                         {:style {:color "green"}})
+               (:task-name (second t))]]
+             [:div.col-sm-2
+              [:button.btn.btn-danger
+               {:id (first t)
+                :on-click #(dispatch [:remove-task (js/parseInt (-> % .-target .-id)) ])}
+               "Delete"]]]])]))))
+
+(defn reagent-examples []
   [:div.container
-   [greeting "Hello world, it is now"]
-   [clock]
-   [color-input]])
-
-
-;; -- Entry Point -------------------------------------------------------------
-
+   [:div.page-header [:h2 "Reagent Examples In Re-frame"]]
+   [counting-component]
+   [:hr]
+   [input-changing]
+   [:hr]
+   [to-do]])
 
 (defn run
   []
   (dispatch-sync [:initialize])
-  (reagent/render [simple-example]
+  (reagent/render [reagent-examples]
                   (js/document.getElementById "app")))
-
 (run)
+
+
